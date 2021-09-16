2560e7376cc5934ef42b9f1b7f09fa53b25155be
merzuleta <merzuleta1@gmail.com>
2021-09-15 20:26:42 -0400

    update

---
+++ src/redux/actions/productsActions.js
@@ -0,0 +1,32 @@
+import { ActionTypes } from "../constants/action-types";
+
+export const setProducts = (products) => {
+  return {
+    type: ActionTypes.SET_PRODUCTS,
+    payload: products,
+  };
+};
+
+export const selectedProduct = (product) => {
+  return {
+    type: ActionTypes.SELECTED_PRODUCT,
+    payload: product,
+  };
+};
+export const removeSelectedProduct = () => {
+  return {
+    type: ActionTypes.REMOVE_SELECTED_PRODUCT,
+  };
+};
+// export const addToCart = (product) => {
+//   return {
+//     type: ActionTypes.ADD_TO_CART,
+//     payload: CART,
+//   };
+// };
+// export const addToCart = (product) => {
+//   return {
+//     type: ActionTypes.ADD_TO_CART,
+//     payload: CART,
+//   };
+// };
